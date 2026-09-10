f945a738d8ea587cdb350316883345a5c69417a6
Lee Xie Loong <2605768@sit.singaporetech.edu.sg>
2026-09-10 19:15:56 +0800

    added failed entry count when the user quits

--- auditor.py
+++ auditor.py
@@ -1,17 +1,22 @@
-inventory = int(0)
+inventory = 0
+failedEntry = 0
 
 while True:
     user = input("Enter a stock quantity: ")
 
     if user == "quit":
+        print("Total Units Processed: " , inventory)
+        print("Total Failed Entries: " , failedEntry)
         break
     if user.isdigit():
         validinventory = int(user)
         inventory += validinventory
     elif user.startswith("-"):
         print("Invalid input, please put in a posititve number.")
+        failedEntry += 1
     else:
         print("Invalid input, please put a number and not spelled in text.")
+        failedEntry += 1
     if inventory > 500:
         print("OVER 500 UNITS!!")
         break

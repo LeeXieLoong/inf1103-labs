ed28d4ba30df68ac19bfe3dd715773e960f23ebf
Lee Xie Loong <2605768@sit.singaporetech.edu.sg>
2026-09-10 19:04:09 +0800

    Inventory will give error if user inputs negative number and text

--- auditor.py
+++ auditor.py
@@ -1,1 +1,23 @@
-print('test')
+inventory = int(0)
+
+while True:
+    user = input("Enter a stock quantity: ")
+
+    if user == "quit":
+        break
+    if user.isdigit():
+        validinventory = int(user)
+        inventory += validinventory
+    elif user.startswith("-"):
+        print("Invalid input, please put in a posititve number.")
+    else:
+        print("Invalid input, please put a number and not spelled in text.")
+        
+    if inventory > 500:
+        print("ALERT! OVER EXREED 500 UNITS!!")
+        break
+    
+
+
+
+

3e1b35f597986876042f5b51100fb0cc3b6af16d
Lee Xie Loong <2605768@sit.singaporetech.edu.sg>
2026-09-17 22:21:01 +0800

    finished lab 3

--- modular_auditor.py
+++ modular_auditor.py
@@ -24,7 +24,29 @@ def generate_report(total_deliveries, failed_attempts):
     print("Total Deliveries Processed:", total_deliveries)
     print("Total Failed Entries:", failed_attempts)
 
+inventory = 0
+failedEntry = 0
+deliveries_processed = 0
+total_tax = 0.0
 
+while True:
+    result = get_valid_input()
+
+    if result == "invalid":
+        failedEntry += 1
+    elif result == "quit":
+        break
+    else:
+        delivery_amount = result
+        inventory = process_delivery(inventory, delivery_amount)
+        total_tax += calculate_tax(delivery_amount)
+        deliveries_processed += 1
+        print("Delivery Processed!")
+        if inventory > 500:
+            print("OVER 500 UNITS!!")
+            break
+
+generate_report(deliveries_processed, failedEntry)
 
 
 
